7c168e31d5cd22a6975fa89f1326bb56d65ad3ea
perogeremmer <mhudyaramadhana@gmail.com>
2021-10-01 09:04:34 +0700

    First Commit

---
+++ src/main/java/com/nyobain/aja/NyobaGUIGIT/NyobaGuiGitApplication.java
@@ -0,0 +1,13 @@
+package com.nyobain.aja.NyobaGUIGIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyobaGuiGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyobaGuiGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nyobain/aja/NyobaGUIGIT/NyobaGuiGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyobain.aja.NyobaGUIGIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyobaGuiGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
